1b0c4f4b31f973bb505c8d3b189d48b4db44aba4
Clara <63094931+sualci@users.noreply.github.com>
2025-04-11 11:29:11 +0200

    Initial commit - creacion del proyecto

---
+++ src/main/java/com/clara/springboot/app/gestor_edificios_api/GestorEdificiosApiApplication.java
@@ -0,0 +1,13 @@
+package com.clara.springboot.app.gestor_edificios_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEdificiosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEdificiosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clara/springboot/app/gestor_edificios_api/GestorEdificiosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clara.springboot.app.gestor_edificios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEdificiosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
